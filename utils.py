55bfb5a56f8af59b618b17400cd0b4421cef8648
Семён Погорельцев <s1m00n@Semens-MacBook-Air.local>
2022-01-27 06:21:35 +0300

    Refactored downloader, close # 6

--- utils.py
+++ utils.py
@@ -1,29 +1,32 @@
-from typing import *
+"""
+Utilities for dowloading the data and other things
+"""
+
 import os
 import requests
 
-T = TypeVar('T')
-T1 = TypeVar('T1')
-T2 = TypeVar('T21')
 
-
-def download(url: str, destination_folder: str):
+def download(url: str, destination_folder: str) -> str:
+    """
+    Downloads the file from url, saves to destination folder
+    :param url: resource url, file extension extracted from it too
+    :param destination_folder: path, where the file is saved
+    :return: filename
+    """
     if not os.path.exists(destination_folder):
         os.makedirs(destination_folder)  # create folder if it does not exist
-
     filename = url.split('/')[-1].replace(" ", "_")  # be careful with file names
     if "?" in filename:
         filename = filename.split('?')[0]
     file_path = os.path.join(destination_folder, filename)
-
-    r = requests.get(url, stream=True)
-    if r.ok:
-        with open(file_path, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024 * 8):
+    ref = requests.get(url, stream=True)
+    if ref.ok:
+        with open(file_path, 'wb') as file:
+            for chunk in ref.iter_content(chunk_size=1024 * 8):
                 if chunk:
-                    f.write(chunk)
-                    f.flush()
-                    os.fsync(f.fileno())
+                    file.write(chunk)
+                    file.flush()
+                    os.fsync(file.fileno())
         return filename
     else:  # HTTP status code 4XX/5XX
-        raise requests.HTTPError("Download failed: status code {}\n{}".format(r.status_code, r.text))
+        raise requests.HTTPError(f"Download failed: status code{ref.status_code}\n{ref.text}")
