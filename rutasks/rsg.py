535ca2ea6809e478b880d649dd60029daf86acad
Семён Погорельцев <s1m00n@Semens-MacBook-Air.local>
2022-01-27 05:41:25 +0300

    mokoron bug

--- rutasks/rsg.py
+++ rutasks/rsg.py
@@ -9,12 +9,14 @@ def preprocess_danetqa(sample: Dict[str, Any]) -> Dict[str, str]:
 
 
 class InputLabelConv:
-    def __init__(self, tokenizer, *tokenizer_args, **tokenizer_kwargs) -> None:
+    def __init__(self, tokenizer, input_name: str = 'input', label_name: str = 'label', *tokenizer_args, **tokenizer_kwargs) -> None:
         self.tokenizer = tokenizer
         self.tokenizer_args = tokenizer_args
         self.tokenizer_kwargs = tokenizer_kwargs
+        self.input_name = input_name
+        self.label_name = label_name
 
     def __call__(self, batch):
-        features = self.tokenizer.batch_encode_plus(batch['input'], *self.tokenizer_args, **self.tokenizer_kwargs)
-        features["labels"] = batch["label"]
+        features = self.tokenizer.batch_encode_plus(batch[self.input_name], *self.tokenizer_args, **self.tokenizer_kwargs)
+        features["labels"] = batch[self.label_name]
         return features
