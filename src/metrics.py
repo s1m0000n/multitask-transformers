fdab6ecc86f1a76f1f0f45f3925c7f12d95321da
Semyon Pogoreltsev <pogorelcevsa@gmail.com>
2022-03-09 16:06:43 +0300

    more classification metrics (worked on #25)

--- src/metrics.py
+++ src/metrics.py
@@ -1,11 +1,11 @@
 import numpy as np
 from typing import Callable, Any, Iterable, Dict
 from dataclasses import dataclass
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
 @dataclass(frozen=True)
-class SequenceClassificationMetric:
+class Metric:
     name: str
     fun: Callable[[np.ndarray, np.ndarray], Any]
 
@@ -15,17 +15,32 @@ class SequenceClassificationMetric:
 
 @dataclass(frozen=True)
 class SequenceClassificationMetrics:
-    metrics: Iterable[SequenceClassificationMetric]
+    metrics: Iterable[Metric]
 
     def __call__(self, logits: np.ndarray, labels: np.ndarray) -> Dict[str, Any]:
         predictions = np.argmax(logits, axis=-1)
         result = {}
         for metric in self.metrics:
-            result[metric.name] = metric(predictions, labels)
+            result[metric.name] = metric(labels, predictions)
         return result
 
 
-accuracy = SequenceClassificationMetric(
+accuracy = Metric(
     name="accuracy",
-    fun=lambda pred, true: accuracy_score(true, pred)
+    fun=accuracy_score
+)
+
+precision = Metric(
+    name="precision",
+    fun=precision_score
+)
+
+recall = Metric(
+    name="recall",
+    fun=recall_score
+)
+
+f1 = Metric(
+    name="f1",
+    fun=f1_score
 )
