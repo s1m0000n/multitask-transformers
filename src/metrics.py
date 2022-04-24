2bb5aa720297b4d2129552f1e9720a66ad13be0e
Semyon Pogoreltsev <pogorelcevsa@gmail.com>
2022-04-25 00:01:25 +0300

    implemented metrics logging + plotting, close #25

--- src/metrics.py
+++ src/metrics.py
@@ -1,41 +1,39 @@
-from typing import Any, Callable, Dict, List, Optional, Tuple, Union, Set
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union, Set, Iterable
 import numpy as np
 import matplotlib.pyplot as plt
-import logging
 
 from src.utils import validate_isinstance
 from dataclasses import dataclass
 
 
-def accuracy(predicted_labels: np.ndarray, actual_labels: np.ndarray, validate: bool = True) -> float:
-    validate_isinstance(predicted_labels, np.ndarray, "predicted_labels", validate=validate)
-    validate_isinstance(actual_labels, np.ndarray, "actual_labels", validate=validate)
-    n_classes = max(len(np.unique(predicted_labels)), len(np.unique(actual_labels)))
-    if n_classes == 1:
-        if predicted_labels[0] == actual_labels[0]:
-            logging.error("Predicted & actual labels contain the same single unique label => accuracy = 1")
-            return 1
-        else:
-            return 0
-    elif n_classes == 2:
-        pass
-    else:
-        pass
-
-
-@dataclass
-class PlotConfig:
-    """
-    :param title: Plot title
-    """
-    title: str = "Metrics Plot"
-    label_x: str = "Steps"
-    label_y: str = "Value"
-    show_legend: bool = True
+@dataclass(frozen=True)
+class MetricConfig:
+    metric: str
+    task: Optional[str] = None
     make_legend_label: Callable[[str, str], str] = lambda task, metric: f"{task} / {metric}"
-    marker: Optional[str] = "o"
     line: Optional[str] = "-"
+    marker: Optional[str] = None
+    color: Optional[str] = None
 
+    @property
+    def label(self) -> str:
+        task = "meta" if self.task is None else self.task
+        return self.make_legend_label(self.task, self.metric)
+
+    def copy(self, **params):
+        return MetricConfig(**dict(self.__dict__, **params))
+
+
+@dataclass(frozen=True)
+class PlotConfig:
+    title: str = "Unnamed plot"
+    show_legend: bool = True
+    xlabel: str = "Steps"
+    ylabel: str = "Value"
+    show: bool = False
+
+    def copy(self, **params):
+        return PlotConfig(**dict(self.__dict__, **params))
 
 
 class MultitaskMetricsLog:
@@ -45,7 +43,6 @@ class MultitaskMetricsLog:
         self.validate = validate
 
     def add(self, task: str, metrics: Dict[str, Any]) -> 'MultitaskMetricsLog':
-        validate_isinstance(metrics, dict, "metrics", validate=self.validate)
         if validate_isinstance(task, str, "task", validate=self.validate) not in self.data:
             self.data[task] = [metrics, ]
         else:
@@ -59,7 +56,15 @@ class MultitaskMetricsLog:
             self.meta[metric_name].append(value)
         return self
 
-    def get(self, task: str, metrics: Optional[Union[str, List[str], Tuple[str], Set[str]]] = None) -> Union[np.ndarray, Dict[str, np.ndarray], List[Dict[str, Any]]]:
+    @property
+    def tasks(self) -> Iterable[str]:
+        return self.data.keys()
+
+    def get(
+            self,
+            task: str,
+            metrics: Optional[Union[str, List[str], Tuple[str], Set[str]]] = None
+    ) -> Union[np.ndarray, Dict[str, np.ndarray], List[Dict[str, Any]]]:
         task_data = self.data[validate_isinstance(task, str, "task")]
         if metrics is None:
             return task_data
@@ -73,23 +78,50 @@ class MultitaskMetricsLog:
         else:
             raise TypeError("Wrong type for 'metrics', must be an instance of str | (list | tuple | set)[str] | None")
 
-    def get_meta(self, metric_names: Union[str, List[str], Tuple[str], Set[str]]) -> Union[np.ndarray, Dict[str, np.ndarray]]:
-        if isinstance(metric_names, str):
-            return np.array(self.meta[metric_names])
-        elif isinstance(metric_names, (list, tuple, set)):
+    def get_meta(
+            self,
+            metrics: Union[str, List[str], Tuple[str], Set[str]]
+    ) -> Union[np.ndarray, Dict[str, np.ndarray]]:
+        if isinstance(metrics, str):
+            return np.array(self.meta[metrics])
+        elif isinstance(metrics, (list, tuple, set)):
             result = {}
-            for metric in metric_names:
+            for metric in metrics:
                 result[metric] = self.get_meta(metric)
             return result
         else:
-             raise TypeError("Wrong type for 'metrics', must be an instance of str | (list | tuple | set)[str]")
+            raise TypeError("Wrong type for 'metrics', must be an instance of str | (list | tuple | set)[str]")
 
-    def plot(self, task_metrics: Dict[str, str], config: Optional[PlotConfig] = None) -> None:
-        config = PlotConfig() if config is None else config
-        # TODO
-        raise NotImplementedError("Implement metrics plotting")
+    def metrics(self, task: Optional[str] = None) -> Iterable[str]:
+        if task is None:
+            return self.meta.keys()
+        task_data = self.get(task)
+        if len(task_data) < 0:
+            raise IndexError("Task log is empty, so no logs available")
+        return self.get(task)[0].keys()
 
-    def plot_meta(self, metric_names: Union[str, List[str], Tuple[str], Set[str]], align: bool = True) -> None:
-        # TODO
-        raise NotImplementedError("Implement meta-metrics plotting")
-
+    def plot(
+        self,
+        *metric_configs: MetricConfig,
+        plot_config: Optional[PlotConfig] = None
+    ) -> None:
+        plot_config = PlotConfig() if plot_config is None else plot_config
+        if len(metric_configs) == 0:
+            metric_configs = [MetricConfig(metric, task) for task in self.tasks for metric in self.metrics(task)]
+            metric_configs += [MetricConfig(metric) for metric in self.metrics()]
+        for cfg in metric_configs:
+            y = self.get_meta(cfg.metric) if cfg.task is None else self.get(cfg.task, cfg.metric)
+            plt.plot(
+                y,
+                marker=cfg.marker if cfg.marker else "",
+                linestyle=cfg.line if cfg.line else "",
+                color=cfg.color,
+                label=cfg.label
+            )
+        plt.xlabel(plot_config.xlabel)
+        plt.ylabel(plot_config.ylabel)
+        plt.title(plot_config.title)
+        if plot_config.show_legend:
+            plt.legend()
+        if plot_config.show:
+            plt.show()
