0a1076157ea128e284001b761852443aaf236663
Semyon Pogoreltsev <pogorelcevsa@gmail.com>
2022-04-24 19:07:26 +0300

    initial metrics logging system

--- src/metrics.py
+++ src/metrics.py
@@ -1,17 +1,50 @@
-from typing import Any, Dict, Iterable, List, Optional, Tuple, Type, Union, Set
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union, Set
 import numpy as np
 import matplotlib.pyplot as plt
+import logging
 
 from src.utils import validate_isinstance
+from dataclasses import dataclass
 
 
-class MetricsLog:
+def accuracy(predicted_labels: np.ndarray, actual_labels: np.ndarray, validate: bool = True) -> float:
+    validate_isinstance(predicted_labels, np.ndarray, "predicted_labels", validate=validate)
+    validate_isinstance(actual_labels, np.ndarray, "actual_labels", validate=validate)
+    n_classes = max(len(np.unique(predicted_labels)), len(np.unique(actual_labels)))
+    if n_classes == 1:
+        if predicted_labels[0] == actual_labels[0]:
+            logging.error("Predicted & actual labels contain the same single unique label => accuracy = 1")
+            return 1
+        else:
+            return 0
+    elif n_classes == 2:
+        pass
+    else:
+        pass
+
+
+@dataclass
+class PlotConfig:
+    """
+    :param title: Plot title
+    """
+    title: str = "Metrics Plot"
+    label_x: str = "Steps"
+    label_y: str = "Value"
+    show_legend: bool = True
+    make_legend_label: Callable[[str, str], str] = lambda task, metric: f"{task} / {metric}"
+    marker: Optional[str] = "o"
+    line: Optional[str] = "-"
+
+
+
+class MultitaskMetricsLog:
     def __init__(self, validate: bool = True) -> None:
         self.data: Dict[str, List[Dict[str, Any]]] = {}
         self.meta = {}
         self.validate = validate
 
-    def add(self, task: str, metrics: Dict[str, Any]) -> 'MetricsLog':
+    def add(self, task: str, metrics: Dict[str, Any]) -> 'MultitaskMetricsLog':
         validate_isinstance(metrics, dict, "metrics", validate=self.validate)
         if validate_isinstance(task, str, "task", validate=self.validate) not in self.data:
             self.data[task] = [metrics, ]
@@ -19,7 +52,7 @@ class MetricsLog:
             self.data[task].append(metrics)
         return self
 
-    def add_meta(self, metric_name: str, value: Any) -> 'MetricsLog':
+    def add_meta(self, metric_name: str, value: Any) -> 'MultitaskMetricsLog':
         if validate_isinstance(metric_name, str, "metric_name", validate=self.validate) not in self.meta:
             self.meta[metric_name] = [value, ]
         else:
@@ -51,9 +84,12 @@ class MetricsLog:
         else:
              raise TypeError("Wrong type for 'metrics', must be an instance of str | (list | tuple | set)[str]")
 
-    def plot(self, task_metrics: Dict[str, str]) -> None:
-        raise NotImplemented("Implement metrics plotting")
+    def plot(self, task_metrics: Dict[str, str], config: Optional[PlotConfig] = None) -> None:
+        config = PlotConfig() if config is None else config
+        # TODO
+        raise NotImplementedError("Implement metrics plotting")
 
-    def plot_meta(self, metric_names: Union[str, List[str], Tuple[str], Set[str]]) -> None:
-        raise NotImplemented("Implement meta-metrics plotting")
+    def plot_meta(self, metric_names: Union[str, List[str], Tuple[str], Set[str]], align: bool = True) -> None:
+        # TODO
+        raise NotImplementedError("Implement meta-metrics plotting")
 
